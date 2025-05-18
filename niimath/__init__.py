616e75f52b959ffa214a6b531715caeafb577333
neurolabusc <rorden@sc.edu>
2025-05-17 23:37:57 -0400

    Use setuptools_scm with write_to for versioning

--- niimath/__init__.py
+++ niimath/__init__.py
@@ -1,16 +1,12 @@
 """Thin wrapper around niimath binary"""
 __author__ = "Casper da Costa-Luis <https://github.com/casperdcl>"
 __date__ = "2025"
-# version detector. Precedence: installed dist, git, 'UNKNOWN'
-try:
-    from ._dist_ver import __version__
-except ImportError: # pragma: nocover
-    try:
-        from setuptools_scm import get_version
 
-        __version__ = get_version(root="../..", relative_to=__file__)
-    except (ImportError, LookupError):
-        __version__ = "UNKNOWN"
+try:
+    from ._dist_ver import version as __version__
+except ImportError:  # pragma: nocover
+    __version__ = "UNKNOWN"
+
 __all__ = ['bin', 'bin_path', 'main']
 
 from pathlib import Path
@@ -31,3 +27,4 @@ def main(args=None, **run_kwargs):
         args = sys.argv[1:]
     from subprocess import run
     return run([bin] + args, **run_kwargs).returncode
+
