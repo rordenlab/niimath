05cf963adfdaa5ddf7c098af154ad996797359c2
Sergey Plis <s.m.plis@gmail.com>
2025-05-18 23:14:03 -0400

    Fix version handling to prevent dev suffix in PyPI packages

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from skbuild import setup
 
 # Get version from SCM (Git tags)
 __version__ = get_version(root=".", relative_to=__file__)
-build_ver = ".".join(__version__.split(".")[:3]).split(".dev")[0]
+# Clean up the version to be just the tag part
+tag_version = __version__.split("+")[0].split(".dev")[0].split(".post")[0]
 
 # Clean problematic pip env entries in CMakeCache
 for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
@@ -16,7 +17,7 @@ for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
 # Build the package with scikit-build and CMake
 setup(
     name="niimath",
-    use_scm_version=True,
+    version=tag_version,
     packages=["niimath"],
     cmake_languages=("C",),
     cmake_minimum_required_version="3.5",
